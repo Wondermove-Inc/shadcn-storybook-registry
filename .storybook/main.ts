fa1e0fe9b16e87507a989c10db7ff9cad4300d47
superman <logi95@gmail.com>
2025-10-11 08:06:34 +0900

    chore: Clean up Storybook config formatting and remove public static dir

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,21 @@
 import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/registry/**/*.mdx",
-    "../src/registry/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/registry/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
-    "@storybook/addon-themes"
+    "@storybook/addon-themes",
   ],
-  "framework": {
-    "name": "@storybook/nextjs-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: [],
 };
 export default config;
